5af36251580961d9202d8b62618ea60224a6267d
Mubarak Shuaib <39945260+Mubaola23@users.noreply.github.com>
2020-06-04 08:52:40 +0100

    Add files via upload

---
+++ scripts/Mubarak Shuaib.py
@@ -0,0 +1,8 @@
+name = "Mubarak Shuaib Olasunkanmi"
+ID ="HNG-01127"
+email ="mubarakshuaib3@gmail.com"
+language ="python"
+
+output = "Hello world, this is " + name +" with HNGi7 ID "+ ID +" and email " + email +" using " + language  + " for stage 2 task "
+print(output)
+
