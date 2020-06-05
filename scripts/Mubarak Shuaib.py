8fc5a07cf30d4dcdefcd266eedc022dd79bf371e
Mubarak Shuaib <39945260+Mubaola23@users.noreply.github.com>
2020-06-05 23:13:57 +0100

    Update Mubarak Shuaib.py

--- scripts/Mubarak Shuaib.py
+++ scripts/Mubarak Shuaib.py
@@ -3,6 +3,5 @@ ID ="HNG-01127"
 email ="mubarakshuaib3@gmail.com"
 language ="python"
 
-output = "Hello world, this is " + name +" with HNGi7 ID "+ ID +" and email " + email +" using " + language  + " for stage 2 task "
+output = "Hello world, this is " + name +" with HNGi7 ID "+ ID +" using " + language  + " for stage 2 task "+email
 print(output)
-
